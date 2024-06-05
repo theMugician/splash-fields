e75fc00714d5fe82c92d75dce86a43feac3c9a58
Greg Slonina <gslonina7@gmail.com>
2024-06-05 10:15:04 -0600

    Select Field

---
+++ src/fields/Select.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Select Class.
+ *
+ * @package splash-fields
+ */
+
+namespace Splash_Fields\Fields;
+
+/**
+ * Class Select.
+ * 
+ */
+class Select extends Input {
+
+	/**
+	 * Get field HTML.
+	 *
+	 * @param mixed $meta   Meta value.
+	 * @param array $field  Field parameters.
+	 *
+	 * @return string
+	 */
+	static public function html( $meta, $field ) {
+        $output     = static::html_label( $field );
+        $output    .= static::html_input( $field, $meta );
+		return $output;
+	}
+	/**
+	 * Get field HTML.
+	 *
+	 * @param   array    $field  Field name.
+	 *
+	 * @return  string
+	 */
+	static public function html_label( $field ) {
+        $field_name = $field['name'];
+        $field_id = $field['id'];
+		$output      =  '<div class="spf-field__label">';
+		$output     .=  sprintf( '<label>%s</label>', $field_name );
+		$output     .=  '</div>';
+		return $output;
+	}
+
+	/**
+	 * Get field HTML.
+	 *
+	 * @param mixed $meta   Meta value.
+	 * @param array $field  Field parameters.
+	 *
+	 * @return string
+	 */
+	static public function html_input( $field, $meta ) {
+		$output     = '<div class="spf-field__input">';
+        $output    .= static::html_select_options( $field, $meta );
+		$output    .= '</div>';
+		return $output;
+    }
+
+	/**
+	 * Get field HTML.
+	 *
+	 * @param mixed $meta   Meta value.
+	 * @param array $field  Field parameters.
+	 *
+	 * @return string
+	 */
+	static public function html_select_options( $field, $meta ) {
+        $attributes = static::get_attributes( $field, $meta );
+        $output = sprintf(
+            '<select %s>',
+            self::render_attributes( $attributes ), 
+        );
+        if ( isset( $field['placeholder'] ) && $field['placeholder'] !== '' ) {
+            $output .= sprintf( '<option>%s</option>', esc_html( $field['placeholder'] ) );
+        }
+        foreach( $field['options'] as $value => $label ) {
+            $selected = '';
+            if ( $value === $meta ) {
+                $selected = ' selected';
+            }
+            $output .= sprintf( '<option%s value="%s">%s</option>', $selected, $value, $label );
+            /*
+            $output .= sprintf( 
+				'<option value="%s" %s %s>', 
+				$value, 
+				self::render_attributes( $attributes ), 
+				checked( $value, $attributes['value'], false )
+			);
+            */
+        }
+        $output .= '</select>';
+		return $output;
+    }
+}
